907f3928c282a6be392f569b3435e2c410418813
Rijk van Zanten <rijkvanzanten@me.com>
2017-02-08 14:08:46 +0100

    :sparkles: wait and see 😏😏

---
+++ assignment5/static/app.js
@@ -0,0 +1,30 @@
+/* global window document */
+
+'use strict';
+
+(function() {
+  const app = {
+    init() {
+      routes.init();
+      sections.init('#best-practices');
+    }
+  };
+
+  const routes = {
+    init() {
+      window.addEventListener('hashchange', () => sections.toggle(window.location.hash));
+    }
+  };
+
+  const sections = {
+    init(route) {
+      document.querySelector(route).classList.add('hidden');
+    },
+    toggle(route) {
+      document.querySelectorAll('section').forEach(section => section.classList.add('hidden'));
+      document.querySelector(route).classList.remove('hidden');
+    }
+  };
+
+  app.init();
+})();
