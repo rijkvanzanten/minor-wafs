d0724a28d0d6803fa9b8fae8319af1f61563776d
Rijk van Zanten <rijkvanzanten@me.com>
2017-02-13 15:48:23 +0100

    :construction: add request for asteroids

--- week2/app.js
+++ week2/app.js
@@ -1,9 +1,42 @@
-/* global fetch */
-import { setBackgroundImage } from './templater.js';
+/* global fetch, document */
+import { component, render } from './templater.js';
 
-fetch('https://api.nasa.gov/planetary/apod?api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6')
+const now = new Date();
+
+// Get and set background-image
+fetch(
+  'https://api.nasa.gov/planetary/apod?api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6',
+)
   .then(res => res.json())
   .then(res => {
-    setBackgroundImage(res);
+    const imageElement = document.querySelector('body > img');
+    imageElement.srcset = `${res.url} 960w, ${res.hdurl} 1200w`;
+    imageElement.addEventListener('load', () =>
+      imageElement.setAttribute('data-loading', false));
   })
   .catch(err => console.error(err)); // TODO: improve error handling
+
+fetch(
+  `https://api.nasa.gov/neo/rest/v1/feed?start_date=${now.getFullYear()}-${addLeadingZero(
+    now.getMonth() + 1,
+  )}-${addLeadingZero(
+    now.getDate(),
+  )}&api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6`,
+)
+  .then(res => res.json())
+  .then(res => {
+    render(
+      document.body,
+      component('section', { class: 'asteroid-list' })(
+        ...Object.keys(res.near_earth_objects).map(date =>
+          component('ol')(
+            ...res.near_earth_objects[date].map(asteroid =>
+              component('li')(asteroid.name)),
+          )),
+      ),
+    );
+  });
+
+function addLeadingZero(number) {
+  return ('0' + number).slice(-2);
+}
