24c5ca638de2b136578ca5ef46cc1ea68793d7c1
Rijk van Zanten <rijkvanzanten@me.com>
2017-02-14 17:37:41 +0100

    :art: improve legibility of code

--- week2/app.js
+++ week2/app.js
@@ -1,53 +1,73 @@
 /* global fetch, document */
+
 import { component, render } from './templater.js';
 
-const now = new Date();
+(function() {
 
-// Get and set background-image
-fetch('https://api.nasa.gov/planetary/apod?api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6')
-  .then(res => res.json())
-  .then(res => {
-    const imageElement = document.querySelector('body > img');
+  const now = new Date();
 
-    // add image urls to sourceset
-    imageElement.srcset = `${res.url} 960w, ${res.hdurl} 1200w`;
+  // Get and set background-image
+  fetch('https://api.nasa.gov/planetary/apod?api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6')
+    .then(res => res.json())
+    .then(res => {
+      const imageElement = document.querySelector('body > img');
 
-    // Add event listener which changes data attribute on image load
-    imageElement.addEventListener('load', () =>
-      imageElement.setAttribute('data-loading', false));
-  })
-  .catch(err => console.error(err)); // TODO: improve error handling
+      // add image urls to sourceset
+      imageElement.srcset = `${res.url} 960w, ${res.hdurl} 1200w`;
 
-fetch(
-  `https://api.nasa.gov/neo/rest/v1/feed?start_date=${now.getFullYear()}-${addLeadingZero(
-    now.getMonth() + 1
-  )}-${addLeadingZero(
-    now.getDate()
-  )}&api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6`
-)
-  .then(res => res.json())
-  .then(res => {
-    render(
-      document.querySelector('.asteroid-list'),
-      ...Object.keys(res.near_earth_objects)
-        .sort((a, b) => new Date(a) - new Date(b))
-        .map(key =>
-          component('li')(
-            component('time', { datetime: key })(new Date(key)),
-            ...res.near_earth_objects[key]
-              .map(asteroid =>
-                component('article', { 'data-hazardous': asteroid.is_potentially_hazardous_asteroid })(
-                  component('a', { href: '/asteroid/' + asteroid.neo_reference_id })(
-                    component('h2')(asteroid.name),
-                    component('p')(asteroid.neo_reference_id)
+      // Add event listener which changes data attribute on image load
+      imageElement.addEventListener('load', () =>
+        imageElement.setAttribute('data-loading', false));
+    })
+    .catch(err => console.error(err)); // TODO: improve error handling
+
+  fetch(
+    `https://api.nasa.gov/neo/rest/v1/feed?start_date=${now.getFullYear()}-${addLeadingZero(
+      now.getMonth() + 1
+    )}-${addLeadingZero(
+      now.getDate()
+    )}&api_key=lNMbOmip78PkrKx5w0VAjKIIAB2zAAGca8DXk2c6`
+  )
+    .then(res => res.json())
+    .then(res => {
+      render(
+        document.querySelector('.asteroid-list'),
+        ...Object.keys(res.near_earth_objects)
+          .sort((a, b) => new Date(a) - new Date(b))
+          .map(key => {
+            const li = component('li');
+            const time = component('time', { datetime: key });
+
+            const articles = res.near_earth_objects[key]
+              .map(asteroid => {
+                const article = component('article', { 'data-hazardous': asteroid.is_potentially_hazardous_asteroid });
+                const a = component('a', { href: '/asteroid/' + asteroid.neo_reference_id });
+                const h2 = component('h2');
+                const p = component('p');
+
+                return (
+                  article(
+                    a(
+                      h2(asteroid.name),
+                      p(asteroid.neo_reference_id)
+                    )
                   )
-                )
-            )
-          )
-      )
-    );
-  });
+                );
+              });
 
-function addLeadingZero(number) {
-  return ('0' + number).slice(-2);
-}
+            return (
+              li(
+                time(new Date(key)),
+                ...articles
+              )
+            );
+          }
+        )
+      );
+    });
+
+  function addLeadingZero(number) {
+    return ('0' + number).slice(-2);
+  }
+
+})();
